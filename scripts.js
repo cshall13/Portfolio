38b83d6a25d096a409e4c6d3483d5f006c085fbf
Shane Hall <cyrus.s.hall84@gmail.com>
2017-06-20 09:58:37 -0400

    updated

--- scripts.js
+++ scripts.js
@@ -1,3 +1,33 @@
 $(document).ready(function(){
 	console.log("work")
+	$('#main-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#main").offset().top
+        }, 700);
+    });
+    $('#projects-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#projects").offset().top
+        }, 700);
+    });
+    $('#skills-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#skills").offset().top
+        }, 700);
+    });
+    $('#about-me-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#about-me").offset().top
+        }, 700);
+    });
+    $('#contact-link').click(function () {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("#contact").offset().top
+        }, 700);
+    });
 });
